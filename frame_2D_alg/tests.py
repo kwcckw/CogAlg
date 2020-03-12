Changes: - Added updates from Khanh - Fix the slicing problem in comp_angle - Added computation of centered pixels in each kernel

frame_2D_alg/tests.py
@@ -6,15 +6,19 @@ Change quickly in parallel with development.
 from time import time
 import operator as op
 from comp_pixel import (
-    comp_pixel, comp_pixel_old,
+    comp_pixel,
+    comp_pixel_old,
+    comp_pixel_ternary,
     comp_3x3, comp_3x3_loop,
 )
 from intra_comp import (
-    translated_operation, angle_diff,
+    angle_diff, translated_operation,
+    calc_a, comp_a,
 )
 from test_sets import (
     comp_pixel_test_pairs,
-    pixels, rderts, gderts,
+    calc_a_test_pairs,
+    pixels, rderts, gderts, angles,
 )
 from utils import *
 
@@ -25,13 +29,39 @@ from utils import *
 # ----------------------------------------------------------------------------
 # Functions
 
-def test_function(func, test_pairs, eval_func=op.eq):
-    """Meta test function. Run tests for a function."""
+def test_function(func, test_pairs, eval_func=op.eq, **kwargs):
+    """
+    Meta test function. Run output tests for a function.
+    Stop if there's an unexpected output.
+    Parameters
+    ----------
+    func : callable
+        The function to test.
+    test_pairs : list of tuples
+        List of inp, out pairs, where out is the expected return
+        value from call of the tested function with inp as input.
+        Type of inp:
+            list, tuple or set : inp is passed as arguments.
+            dict : inp is passed as keyword arguments.
+            other types: inp is passed as a single input.
+    eval_func : callable, optional
+        Function used to check return value against the expected
+        output. eval_func must take at least two required arguments
+        and return a bool.
+    **kwargs
+        Keyword arguments to be passed to eval_func
+    """
     start_time = time()
     print(f'Testing {func.__str__()}... ', end='')
     for inp, out in test_pairs:
-        o = func(*inp)
-        if not eval_func(o, out):
+        if isinstance(inp, (list, tuple, set)):
+            o = func(*inp)
+        elif isinstance(inp, dict):
+            o = func(**inp)
+        else:
+            o = func(inp)
+
+        if not eval_func(o, out, **kwargs):
             print('\n\nTest failed on:')
             print('Input:')
             print(inp)
@@ -61,6 +91,15 @@ def test_is_close(
                 ((np.array([12.1999]), np.array([12.2])), True),
                 (('Dong Thap', 'Dong thap'), False),
                 (('can tho', 'can tho'), True),
+                ((np.nan, np.nan), False),
+                ({'x1':np.nan, 'x2':np.nan, 'equal_nan':True}, True),
+                ((np.array([1.00001, np.nan]),
+                  np.array([1, np.nan])),
+                 False),
+                ({'x1':np.array([1.00001, np.nan]),
+                  'x2':np.array([1, np.nan]),
+                  'equal_nan':True},
+                 True),
         ),
 ):
     """Run tests for is_close function in utils."""
@@ -71,18 +110,25 @@ def test_is_close(
 # Module comp_pixel test functions
 
 def test_comp_pixel(
+        func=comp_pixel,
         test_pairs=comp_pixel_test_pairs,
         eval_func=is_close,  # To check float equality
 ):
     """Test comp_pixels."""
-    test_function(comp_pixel, test_pairs, eval_func)
+    test_function(func, test_pairs, eval_func)
 
 
-def test_comp_3x3_loop(
-        test_pairs=zip(zip(pixels), rderts),
+def test_comp_3x3(
+        test_pairs=zip(pixels, rderts),
         eval_func=is_close,
+        loop=False,
 ):
-    test_function(comp_3x3_loop, test_pairs, eval_func)
+    test_function(comp_3x3_loop if loop else comp_3x3,
+                  test_pairs, eval_func)
+
+# ----------------------------------------------------------------------------
+# Module frame_blobs test functions
+
 
 # ----------------------------------------------------------------------------
 # Module intra_comp test functions
@@ -213,9 +259,15 @@ def test_angle_diff(
     """Test angle_diff."""
     test_function(angle_diff, test_pairs, eval_func)
 
+def test_calc_a(
+        test_pairs=calc_a_test_pairs,
+        eval_func=is_close,
+):
+    test_function(calc_a, test_pairs, eval_func, equal_nan=True)
+
 # ----------------------------------------------------------------------------
 
 if __name__ == "__main__":
     pass
-    # Put function tests here
-    test_comp_pixel()
+    # Put tests here
+    print(comp_a(gderts[2], rng=0, inp=(1, 2, 3)))