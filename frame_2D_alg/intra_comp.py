9ff06ee9f4f8ac0950323591d01f8176d22d2701
ck wei <ck91wei@gmail.com>
2020-03-22 19:11:25 +0800

    Changes: - drafted comp_a - flags to determine type of each derts need further correction - mask need further checking and fixing - adert dimension need further changes

--- frame_2D_alg/intra_comp.py
+++ frame_2D_alg/intra_comp.py
@@ -1,70 +1,13 @@
 """
-Perform comparison of a and then g over predetermined range.
+Cross-comparison of pixels, angles, or gradients, in 2x2 or 3x3 kernels
 """
-
-from itertools import starmap
-
-import operator as op
 
 import numpy as np
 import numpy.ma as ma
 
-from utils import pairwise
-
 # -----------------------------------------------------------------------------
 # Constants
 
-# Slices for vectorized comparison:
-TRANSLATING_SLICES_ = [
-    [
-        (Ellipsis, slice(None, -1, None), slice(None, -1, None)),
-        (Ellipsis, slice(None, -1, None), slice(1, None, None)),
-        (Ellipsis, slice(1, None, None), slice(1, None, None)),
-        (Ellipsis, slice(1, None, None), slice(None, -1, None)),
-    ],
-    [
-        (Ellipsis, slice(None, -2, None), slice(None, -2, None)),
-        (Ellipsis, slice(None, -2, None), slice(1, -1, None)),
-        (Ellipsis, slice(None, -2, None), slice(2, None, None)),
-        (Ellipsis, slice(1, -1, None), slice(2, None, None)),
-        (Ellipsis, slice(2, None, None), slice(2, None, None)),
-        (Ellipsis, slice(2, None, None), slice(1, -1, None)),
-        (Ellipsis, slice(2, None, None), slice(None, -2, None)),
-        (Ellipsis, slice(1, -1, None), slice(None, -2, None)),
-    ],
-]
-TRANSLATING_SLICES_PAIRS_ = [
-    (  # rng = 0 or 2x2
-        (
-            (Ellipsis, slice(None, -1, None), slice(None, -1, None)),
-            (Ellipsis, slice(1, None, None), slice(1, None, None)),
-        ),
-        (
-            (Ellipsis, slice(None, -1, None), slice(1, None, None)),
-            (Ellipsis, slice(1, None, None), slice(None, -1, None)),
-        ),
-    ),
-    (  # rng = 1 or 3x3
-        (
-            (Ellipsis, slice(None, -2, None), slice(None, -2, None)),
-            (Ellipsis, slice(2, None, None), slice(2, None, None)),
-        ),
-        (
-            (Ellipsis, slice(None, -2, None), slice(1, -1, None)),
-            (Ellipsis, slice(2, None, None), slice(1, -1, None)),
-        ),
-        (
-            (Ellipsis, slice(None, -2, None), slice(2, None, None)),
-            (Ellipsis, slice(2, None, None), slice(None, -2, None)),
-        ),
-        (
-            (Ellipsis, slice(1, -1, None), slice(2, None, None)),
-            (Ellipsis, slice(1, -1, None), slice(None, -2, None)),
-        ),
-    ),
-]
-
-# coefficients for decomposing d into dy and dx:
 
 Y_COEFFS = [
     np.array([-1, -1, 1, 1]),
@@ -79,203 +22,234 @@ X_COEFFS = [
 # -----------------------------------------------------------------------------
 # Functions
 
-def comp_g(dert__, odd):
-    g__ = dert__[0]
-    a__ = dert__[1:]
-
-    # loop through each pair of comparands in a kernel
-    dgy__ = ma.zeros(np.subtract(g.shape, rng))
-    dgx__ = ma.zeros(np.subtract(g.shape, rng))
-    for x_coeff, y_coeff, (ts, _ts) in zip(X_COEFFS[rng],
-                                           Y_COEFFS[rng],
-                                           TRANSLATING_SLICES_PAIRS_[rng]):
-        # find angle differences
-        da__ = angle_diff(a__[ts], a__[_ts])
-
-        # compute dg: dg = g - _g * cos(da) at each position
-        dg__ = g__[ts] - g__[_ts] * da__[1]
-
-        # accumulate dgy, dgx
-        dgx__ += dg__ * x_coeff
-        dgy__ += dg__ * y_coeff
-
-    gg__ = ma.hypot(dgy__, dgx__)
-
-    return ma.stack((g__, gg__, dgy__, dgx__))
+def comp_g(dert__, fc3):
+    """
+    Cross-comp of g or ga, in 2x2 kernels unless root fork is comp_r: fc3=True, sparse 3x3 comp
+    Parameters
+    ----------
+    dert__ : array-like
+        The structure is (i, g, dy, dx) for dert or (ga, day, dax) for adert.
+    fc3 : bool
+        Initially False, set to True for comp_a and comp_g called from comp_r fork.
+    Returns
+    -------
+    gdert__ : masked_array
+        Output's structure is (g, gg, gdy, gdx, gm, ga, day, dax).
+    Examples
+    --------
+    >>> # actual python console code
+    >>> dert__ = 'specific value'
+    >>> fc3 = 'specific value'
+    >>> comp_g(dert__, fc3)
+    'specific output'
+    Notes
+    -----
+    Comparand is dert[1]
+    """
+    pass
 
 
 def comp_r(dert__, fig):
-
-    i__, g__, dy__, dx__ = dert__[:]
-    dy__ = dy__[:, rng:-rng, rng:-rng]
-    dx__ = dx__[:, rng:-rng, rng:-rng]
-
-    # comparison
-    d__ = translated_operation(i__, rng=rng, operator=op.sub)
-
-    # sum within kernels
-    dy__ += (d__ * Y_COEFFS[rng]).sum(axis=-1)
-    dx__ += (d__ * X_COEFFS[rng]).sum(axis=-1)
-
-    # compute gradient magnitudes
-    g__ = ma.hypot(dy__, dx__)
-
-    return ma.stack((i__, g__, dy__, dx__))
-
-
-def comp_a(dert__, inp):
     """
-    Compare angles within specified rng.
+    Cross-comp of input param (dert[0]) over rng set in intra_blob.
+    This comparison is selective for blobs with below-average gradient,
+    where input intensity doesn't vary much in shorter-range cross-comparison.
+    Such input is predictable enough for selective sampling: skipping
+    alternating derts as a kernel-central dert at current comparison range,
+    which forms increasingly sparse input dert__ for greater range cross-comp,
+    while maintaining one-to-one overlap between kernels of compared derts.
+    With increasingly sparse input, unilateral rng (distance between
+    central derts) can only increase as 2^(n + 1), where n starts at 0:
+    With increasingly sparse input, unilateral rng (distance between
+    central derts) can only increase as 2^(n + 1), where n starts at 0:
+    rng = 1 : 3x3 kernel, skip orthogonally alternating derts as centrals,
+    rng = 2 : 5x5 kernel, skip diagonally alternating derts as centrals,
+    rng = 3 : 9x9 kernel, skip orthogonally alternating derts as centrals,
+    ...
+    That means configuration of preserved (not skipped) derts will always be 3x3.
     Parameters
     ----------
-    dert__ : masked_array
-        Contain input arrays.
-    rng : int
-        Chebyshev distance between the comparands.
-    inp : tuple
-        Index/indices of input arrays.
+    dert__ : array-like
+        dert's structure is (i, g, dy, dx, m).
+    fig : bool
+        True if input is g.
     Returns
     -------
-    adert__ : masked_array
-        Array of shape (5, <height>, <width>).
-        - adert__[0, :, :] : sine inputs' (gradient) angles.
-        - adert__[1, :, :] : cosine inputs' (gradient) angles.
-        - adert__[2, :, :] : magnitude of angle derivation (ga).
-        - adert__[3, :, :] : sines of angle differences by x axis.
-        - adert__[4, :, :] : cosines of angle differences by x axis.
-        - adert__[5, :, :] : sines of angle differences by y axis.
-        - adert__[6, :, :] : cosines of angle differences by y axis.
+    rdert__ : masked_array
+        Output's structure is (i, g, dy, dx, m).
+    Examples
+    --------
+    >>> # actual python console code
+    >>> dert__ = 'specific value'
+    >>> fig = 'specific value'
+    >>> comp_r(dert__, fig)
+    'specific output'
+    Notes
+    -----
+    - Results are accumulated in the input dert.
+    - Comparand is dert[0].
     """
-    if isinstance(inp, (list, tuple, set)):
-        inp = list(inp)
-    else:
-        raise ValueError("'inp' should be a tuple of integers).")
-    if len(inp) == 3:
-        a__ = calc_a(dert__, inp)
-    elif len(inp) == 5:
-        a__ = calc_aga(dert__, inp)
-    else:
-        raise ValueError("'inp' should contain the index/indices "
-                         "for g, dy, dx in that order.")
-    return comp_angle(a__,inp)
+    pass
 
 
-def calc_a(dert__, inp):
-    """Compute angles of gradient."""
-    return dert__[inp[1:]] / dert__[inp[0]]
-
-
-def calc_aga(dert__, inp):
-    """Compute angles of angle gradients."""
-    g__ = dert__[inp[1]]
-    day__ = np.arctan2(*dert__[inp[1:3]])
-    dax__ = np.arctan2(*dert__[inp[3:]])
-    return np.stack((day__, dax__)) / g__
-
-
-def comp_angle(a__,inp):
-    """Compare angles."""
-
-    # compute rng based on inp
-    rng = 0 if len(inp) == 3 else 1
-
-    # handle mask
+def comp_a(dert__, fga, fc3):
+    """
+    cross-comp of a or aga, in 2x2 kernels unless root fork is comp_r: fc3=True.
+    Parameters
+    ----------
+    dert__ : array-like
+        dert's structure depends on fga
+    fc3 : bool
+        Is True if root fork is comp_r.
+    fga : bool
+        If True, dert's structure is interpreted as:
+        (g, gg, gdy, gdx, gm, iga, iday, idax)
+        Otherwise it is interpreted as:
+        (i, g, dy, dx, m)
+    Returns
+    -------
+    adert : masked_array
+        adert's structure is (ga, day, dax).
+    Examples
+    --------
+    >>> # actual python console code
+    >>> dert__ = 'specific value'
+    >>> odd = 'specific value'
+    >>> aga = 'specific value'
+    >>> comp_a(dert__, fga, fc3)
+    'specific output'
+    """
+        
+    # input is rdert (ir, gr, dry, drx, mr)
+    if fc3:
+        gr,dry,drx = dert__[1:4,:]
+        a__ = [dry,drx]/gr
+        a__ = a__[:,::2,::2]  # skip odd angle`
+        
+    # input is gdert (g, gg, gdy, gdx, gm, iga, iday, idax)
+    elif fga:
+        gg,gdy,gdx = dert__[1:4,:]
+        a__ = [gdy,gdx]/gg
+        
+#    # input is dert (i, g, dy, dx, m)
+#    elif fig:
+#        g,dy,dx = dert__[1:4,:]
+#        a__ = [gdy,gdx]/gg
+      
+    # input is adert (ga, day, dax)
+    else :
+        ga,day,dax = dert__[0:3,:]
+        a__ = [day,dax]/ga
+    
     if isinstance(a__, ma.masked_array):
         a__.data[a__.mask] = np.nan
         a__.mask = ma.nomask
+      
+    # input is rdert,rng = 1
+    if fc3:
+        
+        # each shifted a
+        a__topleft      = a__[:,:-2,:-2]
+        a__top          = a__[:,:-2,1:-1]
+        a__topright     = a__[:,:-2,2:]
+        a__right        = a__[:,1:-1,2:]
+        a__bottomright  = a__[:,2:,2:]
+        a__bottom       = a__[:,2:,1:-1]
+        a__bottomleft   = a__[:,2:,:-2]
+        a__left         = a__[:,1:-1,:-2]
+        
+        # central of a
+        a__= a__[:,1:-1,1:-1]
+        
+        # get angle difference of each direction
+        da__ = np.stack((angle_diff(a__,a__topleft),
+                         angle_diff(a__,a__top),
+                         angle_diff(a__,a__topright),
+                         angle_diff(a__,a__right),
+                         angle_diff(a__,a__bottomright),
+                         angle_diff(a__,a__bottom),
+                         angle_diff(a__,a__bottomleft),
+                         angle_diff(a__,a__left)));
+        
+        da__ = np.rollaxis(da__,0,4)               
+           
+        # compute day and dax              
+        day__ = (da__ * Y_COEFFS[1]).sum(axis=-1)
+        dax__ = (da__ * X_COEFFS[1]).sum(axis=-1)
+        
+        # compute gradient magnitudes (how fast angles are changing)
+        ga__ = np.hypot(np.arctan2(*day__), np.arctan2(*dax__))
+        
+    # input is dert, gdert or adert,rng = 0
+    else:
+        
+        # each shifted a
+        a__topleft = a__[:,:-1,:-1]
+        a__topright = a__[:,:-1,1:]
+        a__bottomright = a__[:,1:,1:]
+        a__bottomleft = a__[:,1:,:-1]
+        
+        # central of a
+        a__ = (a__topleft + a__topright + a__bottomright + a__bottomleft)/4
+        
+        # get angle difference of each direction
+        da__ = np.stack((angle_diff(a__,a__topleft),
+                         angle_diff(a__,a__topright),
+                         angle_diff(a__,a__bottomright),
+                         angle_diff(a__,a__bottomleft)))
+            
+        da__ = np.rollaxis(da__,0,4)    
+            
+        # compute day and dax
+        day__ = (da__ * Y_COEFFS[0]).sum(axis=-1)
+        dax__ = (da__ * X_COEFFS[0]).sum(axis=-1)
+        
+        # compute gradient magnitudes (how fast angles are changing)
+        ga__ = np.hypot(np.arctan2(*day__), np.arctan2(*dax__))
 
-    # comparison
-    da__,a__ = translated_operation(a__, rng=rng, operator=angle_diff)
 
-    # sum within kernels
-    day__ = (da__ * Y_COEFFS[rng]).sum(axis=-1)
-    dax__ = (da__ * X_COEFFS[rng]).sum(axis=-1)
+    adert = ma.stack((ga__,*day__,*dax__))
 
-    # compute gradient magnitudes (how fast angles are changing)
-    ga__ = np.hypot(np.arctan2(*day__), np.arctan2(*dax__))
+    return adert
 
-    # pack into dert
-    dert__ = ma.stack((*a__, ga__, *day__, *dax__), axis=0)
 
-    # handle mask
-    dert__.mask = np.isnan(dert__.data)
-
-    return dert__
+def calc_a(dert__):
+    """
+    Compute vector representation of gradient angle.
+    It is done by normalizing the vector (dy, dx).
+    Numpy broad-casting is a viable option when the
+    first dimension of input array (dert__) separate
+    different CogAlg parameter (like g, dy, dx).
+    Example
+    -------
+    >>> dert1 = np.array([0, 5, 3, 4])
+    >>> a1 = calc_a(dert1)
+    >>> print(a1)
+    array([0.6, 0.8])
+    >>> # 45 degrees angle
+    >>> dert2 = np.array([0, 450**0.5, 15, 15])
+    >>> a2 = calc_a(dert2)
+    >>> print(a2)
+    array([0.70710678, 0.70710678])
+    >>> print(np.degrees(np.arctan2(*a2)))
+    45.0
+    >>> # -30 (or 330) degrees angle
+    >>> dert3 = np.array([0, 10, -5, 75**0.5])
+    >>> a3 = calc_a(dert3)
+    >>> print(a3)
+    array([-0.5      ,  0.8660254])
+    >>> print(np.rad2deg(np.arctan2(*a3)))
+    -29.999999999999996
+    """
+    return dert__[[2, 3]] / dert__[1]  # np.array([dy, dx]) / g
 
 
 # -----------------------------------------------------------------------------
 # Utility functions
 
-def translated_operation(a, rng, operator):
-    """
-    Return an array of corresponding results from operations between
-    diametrically opposed translated slices.
-    Parameters
-    ----------
-    a : array-like
-        Input array.
-    rng : int
-        Half of the Chebyshev distance between the two inputs
-        in each pairs.
-    operator : function
-        Binary operator used to compute results
-    Return
-    ------
-    out : MaskedArray
-        Array of results where additional dimension correspondent
-        to each pair of translated slice.
-    """
-
-
-    # get centered pixels for 2x2 and 3x3 based on rng
-    a__ = a[:,1:-1,1:-1] if rng != 0 \
-    else (a[:,:-1,:-1]+a[:,:-1,1:]+a[:,1:,1:]+a[:,1:,:-1]) /4
-
-    # compute angle difference
-    out = ma.masked_array([operator(a__, a[ts]) for ts in
-                           TRANSLATING_SLICES_[rng]])
-
-    # Rearrange axes:
-    for dim1, dim2 in pairwise(range(out.ndim)):
-        out = out.swapaxes(dim1, dim2)
-
-    return out,a__
-
-
-def translated_array(a, rng):
-    """
-    Like translated_operation, but without applying operation to slices
-    """
-    out = ma.stack([a[ts] for ts in TRANSLATING_SLICES_[rng]])
-
-    # Rearrange axes:
-    for dim1, dim2 in pairwise(range(out.ndim)):
-        out = out.swapaxes(dim1, dim2)
-
-    return out
-
-
-def central_slice(k):
-    """Return central slice objects (last 2 dimensions)."""
-    if k < 1:
-        return ..., slice(None), slice(None)
-    return ..., slice(k, -k), slice(k, -k)
-
-
-def rim_mask(shape, i):
-    """
-    Return 2D array mask where outer pad (pad width=i) is True,
-    the rest is False.
-    """
-    out = np.ones(shape, dtype=bool)
-    out[central_slice(i)] = False
-    return out
-
-
 def angle_diff(a2, a1):
     """
-    Return the vector, of which angle is the angle between a2 and a1.
-    Note: This only works for angle in 2D space.
+    Return the sine(s) and cosine(s) of the angle between a2 and a1.
     Parameters
     ----------
     a1 , a2 : array-like
@@ -287,6 +261,9 @@ def angle_diff(a2, a1):
     out : MaskedArray
         The first dimension is sine/cosine of the angle(s) between
         a2 and a1.
+    Note
+    ----
+    This only works for angles in 2D space.
     """
     return ma.array([a1[1] * a2[0] - a1[0] * a2[1],
                      a1[0] * a2[0] + a1[1] * a2[1]])
@@ -304,3 +281,14 @@ def angle_diff(a2, a1):
 
 # ----------------------------------------------------------------------
 # -----------------------------------------------------------------------------
+    
+    
+#from test_sets import (
+#comp_pixel_test_pairs,
+#calc_a_test_pairs,
+#pixels, rderts, gderts, angles)
+#
+#dert__ = gderts[2]
+#fga = 0
+#fc3 = 0
+#comp_a(dert__, fga, fc3)
