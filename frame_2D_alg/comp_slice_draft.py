5b9430ac2438660f6db5dc93512ffc4c96b18a09
kwcckw <ck91wei@gmail.com>
2021-02-12 16:59:08 +0800

    Changes:
    - minor edits and some fixes

--- frame_2D_alg/comp_slice_draft.py
+++ frame_2D_alg/comp_slice_draft.py
@@ -68,6 +68,7 @@ class CPP(ClusterStructure):
 class CSstack(ClusterStructure):
     # an element of sstack in PP
     upconnect_ = list
+    upconnect_cnt = int
     downconnect_cnt = int
     #+ other params of CStack?
     Py_ = list
@@ -94,9 +95,10 @@ def comp_slice_(stack_, _P):
             stack.f_checked = 1
             DdX = 0  # accumulate across stacks?
 
-            if not _P:  # stack_ = upconnect_, else stack_ = blob.stack_
+            dert_Py_ = []
+            if not _P: # stack is from blob.stack_
                 _P = stack.Py_.pop()
-            dert_Py_ = [Cdert_P(Pi=_P)]  # _P only, no derivatives in 1st dert_P
+                dert_Py_.append(Cdert_P(Pi=_P))  # _P only, no derivatives in 1st dert_P
 
             for P in reversed(stack.Py_):
                 dert_P = comp_slice(P, _P, DdX)  # ortho and other conditional operations are evaluated per PP
@@ -154,7 +156,7 @@ def comp_slice(P, _P, DdX):  # forms vertical derivatives of P params, and condi
     # correlation: dX -> L, oDy, !oDx, ddX -> dL, odDy ! odDx? dL -> dDx, dDy?  G = hypot(Dy, Dx) for 2D structures comp?
     Pm = mX + mL + mM + mDx + mDy + mMg + mDg # -> complementary vPP, rdn *= Pd | Pm rolp?
 
-    dert_P = Cdert_P(Pi=_P, Pm=Pm, Pd=Pd, mX=mX, dX=dX, mL=mL, dL=dL, mDx=mDx, dDx=dDx, mDy=mDy, dDy=dDy, mDg=mDg, dDg=dDg, mMg=mMg, dMg=dMg)
+    dert_P = Cdert_P(Pi=P, Pm=Pm, Pd=Pd, mX=mX, dX=dX, mL=mL, dL=dL, mDx=mDx, dDx=dDx, mDy=mDy, dDy=dDy, mDg=mDg, dDg=dDg, mMg=mMg, dMg=dMg)
     # div_f, nvars
 
     return dert_P
@@ -221,15 +223,8 @@ def upconnect_2_PP_(stack_, PP_, iPP, isstack, _dert_P):  # terminate, initializ
 
     for i, stack in enumerate(stack_):  # breadth-first, upconnect_ is not reversed
         dert_P = stack.Py_[0]
-
-        # get x start and end point
-        _x0 = _dert_P.Pi.x0
-        _xn = _dert_P.Pi.x0+_dert_P.Pi.L
-        x0 = dert_P.Pi.x0
-        xn = dert_P.Pi.x0+dert_P.Pi.L
-        
-        # upconnect with same sign and having at least 1 contact point
-        if (_dert_P.Pm > 0) == (dert_P.Pm > 0) and (_xn>=x0) and (_x0 <= xn):
+        # upconnect with same sign
+        if (_dert_P.Pm > 0) == (dert_P.Pm > 0):
             upconnect_.append(stack_.pop(i))  # separate stack_ into sign-connected stacks: upconnect_, and unconnected stacks: popped stack_
 
     if len(upconnect_) == 1:  # 1 same-sign upconnect per PP
@@ -240,9 +235,10 @@ def upconnect_2_PP_(stack_, PP_, iPP, isstack, _dert_P):  # terminate, initializ
 
             for dert_P in upconnect_[0].Py_:
                 if (_dert_P.Pm > 0) != (dert_P.Pm > 0):
+                    isstack.upconnect_cnt += 1 # increase upconnect count when terminated in upconnect section
                     accum_PP(upconnect_[0], isstack, PP)
                     PP_.append(PP)  # terminate sstack and PP
-                    isstack = CSstack(dert_Pi=Cdert_P())  # init empty sstack, then accum_sstack
+                    isstack = CSstack(dert_Pi=Cdert_P(), downconnect_cnt=1)  # init empty sstack (downconnect start with 1 in upconnect loop), then accum_sstack
                     PP = CPP(sstacki=CSstack(dert_Pi=Cdert_P()))
                     isstack.PP = PP
 
@@ -250,9 +246,8 @@ def upconnect_2_PP_(stack_, PP_, iPP, isstack, _dert_P):  # terminate, initializ
                 _dert_P = dert_P
 
         # merge contacted PPs
-        elif (upconnect_[0].sstack_[0].PP is not isstack.PP):
-            merge_PP(upconnect_[0].sstack_[0].PP, isstack.PP, PP_)
-            
+        else:
+            merge_PP(upconnect_[0].sstack_[0].PP, iPP, PP_)
             
     elif upconnect_:  # >1 same-sign upconnects per PP
         idert_P = _dert_P     # downconnected dert_P
@@ -264,32 +259,33 @@ def upconnect_2_PP_(stack_, PP_, iPP, isstack, _dert_P):  # terminate, initializ
             PP = iPP  # then redefined per stack
             _dert_P = idert_P
             
-            if not upconnect_[0].sstack_: 
+            if not upconnect.sstack_:
                 for dert_P in upconnect.Py_:
                     if (_dert_P.Pm > 0) != (dert_P.Pm > 0):
+                        sstack.upconnect_cnt += 1 # increase upconnect count when terminated in upconnect section
                         accum_PP(upconnect, sstack, PP)  # term. sstack
-                        if PP is iPP:  # separate iPP termination test
+                        if (PP is iPP) and (curr_upconnect_cnt-1 == 0):  # separate iPP termination test
                             curr_upconnect_cnt -= 1
-                            if curr_upconnect_cnt == 0:
-                                PP_.append(PP)
+                            PP_.append(PP)
                         else:  # terminate stack-local PP
                             PP_.append(PP)
                         # init PP regardless of iPP:
-                        sstack = CSstack(dert_Pi=Cdert_P())  # init empty
+                        sstack = CSstack(dert_Pi=Cdert_P(), downconnect_cnt=1)  # init empty
                         PP = CPP(sstacki=CSstack(dert_Pi=Cdert_P()))  # we don't know if PP will fork at stack term
                         sstack.PP = PP
 
                     accum_sstack(sstack, dert_P)  # regardless of termination
                     _dert_P = dert_P
+                    
                 upconnect_2_PP_(upconnect.upconnect_, PP_, PP, sstack, _dert_P)
 
-            # ffirst may not need now, all upconnects are checked and having direct contact to iPP before forming them
-            elif (upconnect_[0].sstack_[0].PP  is not isstack.PP):
-                    merge_PP(upconnect_[0].sstack_[0].PP , isstack.PP, PP_)
+            else:
+                merge_PP(upconnect.sstack_[0].PP , iPP, PP_)
 
     else:  # 0 same-sign upconnects per PP:
         accum_PP([], isstack, iPP)  # accumulate sstack into PP
         PP_.append(iPP)
+        
 
     stack_2_PP_(stack_, PP_)  # stack_ now contains only stacks unconnected to isstack
 
