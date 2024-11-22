1b483ff1bbfd0c3f33954ee0d22e9ac3bf0dc6d9
Kok Wei <52521979+kwcckw@users.noreply.github.com>
2024-11-22 15:04:11 +0800

    Changes:
    1. Edits in agg_recursion.

--- frame_2D_alg/vectorize_edge_blob/agg_recursion.py
+++ frame_2D_alg/vectorize_edge_blob/agg_recursion.py
@@ -18,6 +18,7 @@ def agg_cluster_(frame):  # breadth-first (node_,L_) cross-comp, clustering, rec
 
         pL_ = {l for n in N_ for l,_ in get_rim(n, fd)}
         if len(pL_) > ave_L:
+            for N in N_: N.root_ = []  # reset for cluster_N_
             G_ = cluster_N_(frame, pL_, fd)  # optionally divisive clustering
             frame.subG_ = G_
             if len(G_) > ave_L:
@@ -123,15 +124,15 @@ def med_cluster_(frame):
 
     def medoid(N_):  # sum and average nodes
 
-        N = CG(root_=[None])
-        for n in N_:
-            N.n += n.n; N.rng = n.rng; N.aRad += n.aRad
+        N = CG(); N.M = 0  # we may need medoid M too?
+        for n, m in N_:
+            N.n += n.n; N.rng = n.rng; N.aRad += n.aRad; N.M += n.M
             N.box = extend_box(N.box, n.box)
             N.latuple += n.latuple; N.mdLay += n.mdLay
             N.derH.add_H(n.derH); N.extH.add_H(n.extH)
         # get averages:
         k = len(N_)
-        N.n /= k; N.latuple /= k; N.mdLay /= k; N.aRad /= k; N.derH.norm_(k)  # derH/= k
+        N.n /= k; N.latuple /= k; N.mdLay /= k; N.aRad /= k; N.M/=k; N.derH.norm_(k)  # derH/= k
         return N
 
     def xcomp_(N_):  # initial global cross-comp
@@ -143,7 +144,7 @@ def med_cluster_(frame):
             vM = M - ave
             for _g,g in (_G,G),(G,_G):
                 if vM > 0:
-                    g.perim.add(_g)  # loose match
+                    g.perim.add((_g,M))  # loose match
                     if vM > ave:
                         g.crim.add((_g,M))  # strict match
                         g.M += M
@@ -153,8 +154,11 @@ def med_cluster_(frame):
         exemplar_ = []
         for N in N_:
             for _N, M in N.crim:
+                '''
                 if N.M >_N.M: _N.M -= M
                 else:          N.M -= M
+                '''
+                if N.M <= _N.M: N.M -= M  # this shouldn't be bidirectional? Because in _N loop later, they will be eval the smae too
                 # exclusive representation, or incr N.Mr?
             if N.M > ave:
                 exemplar_ += [N]
@@ -170,9 +174,11 @@ def med_cluster_(frame):
             mN = medoid(_node_)
             for _N,_m in _peri_:
                 m = comp_cN(mN,_N)
-                if M > ave:
-                    peri_.add((_N,m))
-                    if M > ave*2: node_.add((_N,m))
+                M += m
+                if m > ave:  # should be m here? Because M should be accumulating m>
+                    peri_.add((_N,_m))  # should be _m here?
+                    if m > ave*20: # m value is too large if we use ave to eval here, so it's just evaluating positive and negative m now, is there a better eval method?
+                        node_.add((_N,_m))
             dM = M - _M
             _node_,_peri_,_M = node_,peri_,M
 
@@ -180,14 +186,15 @@ def med_cluster_(frame):
 
     N_ = frame.subG_  # should be complemented graphs: m-type core + d-type contour
     for N in N_:
-        N.perim = set(); N.crim = set(); N.root_ += [frame]
+        N.perim = set(); N.crim = set(); N.root_ += [frame]; N.M = 0
     xcomp_(N_)
-    exemplar_ = get_exemplar_(N_)  # select strong Ns
+    exemplar_ = get_exemplar_(N_)  # select strong Ns (some strong Ns might be in some exemplar's crim too, so there will be overlapping?)
     for N in exemplar_:
         if N.M > ave * 10:  # tentative, else keep N.crim
             refine_(N)  # N.crim = N.perim clustered by N.crim medoid
     frame.exemplar_ = exemplar_
     if len(frame.exemplar_) > ave_L:
+        frame.subG_ = frame.exemplar_  # for next recursion
         agg_cluster_(frame)  # alternating connectivity clustering per exemplar, more selective
 
 
