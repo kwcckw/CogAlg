1ca9754936623f968d8f8d4e2fb5e83342627a63
Kok Wei <52521979+kwcckw@users.noreply.github.com>
2025-01-24 15:54:28 +0800

    Changes:
    1. Edits in vect_edge.

--- frame_2D_alg/vect_edge.py
+++ frame_2D_alg/vect_edge.py
@@ -369,7 +369,7 @@ def comp_N(_N,N, rn, angle=None, dist=None, dir=1):  # dir if fd, Link.derH=dH, 
         # same olp?
     Link = CL(fd=fd, nodet=[_N,N], yx=np.add(_N.yx,N.yx)/2, angle=angle, dist=dist, box=extend_box(N.box,_N.box))
     lay0 = CLay(root=Link, Et=Et, m_d_t=[m_t,d_t], node_=_N.node_+N.node_, link_=_N.link_+N.link_)  # remove overlap later
-    derH = comp_H(_N.derH, N.derH, rn, Link, Et)  # comp shared layers, if any
+    derH = comp_H(_N.derH, N.derH, fd, rn, Link, Et)  # comp shared layers, if any
     Link.derH = [lay0, *derH]
     # spec: comp_node_(node_|link_), combinatorial, node_ nested / rng-)agg+?
     if not fd and _N.altG and N.altG:  # if alt M?
@@ -429,38 +429,55 @@ def sum2graph(root, grapht, fd, minL=0, maxL=None):  # sum node and link params 
 def sum_H(Q, root, rev=0, fc=0, fd=0):  # sum derH in link_|node_
 
     DerH = []
-    for lay in Q[0].derH:  # 2 forks per layer
-        DerH += [[fork.copy_(root=root,rev=rev,fc=fc) if fork else [] for fork in lay]]
-    for e in Q[1:]:
-        add_H(DerH, e.derH, root, rev=0, fc=0, fd=fd)
+    for e in Q:
+        add_H(DerH, e.derH, root, rev=0, fc=0, fd=fd)  #  single fork in lay0  and 2 forks per higher layer in each e
 
     return DerH  # list
 
 def add_H(H, h, root, rev=0, fc=0, fd=0):
 
-    for Lay, lay in zip_longest(H, h, fillvalue=[]):  # different len if lay-selective comp
+    for i, (Lay, lay) in enumerate(zip_longest(H, h, fillvalue=[])):  # different len if lay-selective comp
         if lay:
             if Lay:
-                for fork in lay: Lay[fork].add_lay(fork,rev=rev,fc=fc)  # combine input forks
+                if i:
+                    for i, (Fork, fork) in enumerate(zip(Lay, lay)): 
+                        if fork:
+                            if Fork: Fork.add_lay(fork,rev=rev,fc=fc)  
+                            else:    Lay[i] = fork.copy_(root=root)  # replaces [] with fork  
+                else:  # lay0 single CLay
+                    Lay.add_lay(lay)  
             else:
-                L = CLay(root=root)
-                for fork in lay:
-                    if fork:
-                        root.Et += fork.Et; L.add_lay(fork.copy_(root=root,rev=rev,fc=fc))
-                H += [[[],L] if fd else [L,[]]]
+                if i:
+                    Lay = []
+                    for fork in lay:
+                        if fork: 
+                            fork = fork.copy_(root=root, rev=rev, fc=fc)
+                            root.Et += fork.Et
+                        else:    
+                            fork = []  # empty list
+                        Lay += [fork]
+                else:  # lay0 single CLay
+                    Lay = lay.copy_(root=root, rev=rev, fc=fc)
+                    root.Et += Lay.Et
+                H += [Lay]
 
-def comp_H(H, h, rn, root, Et):
+def comp_H(H, h, fd, rn, root, Et):
 
     derH = []
-    for _lay, lay in zip_longest(H, h, fillvalue=[]):  # different len if lay-selective comp
+    for i, (_lay, lay) in enumerate(zip_longest(H, h, fillvalue=[])):  # different len if lay-selective comp
         if _lay and lay:
-            dLay = []
-            for _fork, fork in zip_longest(_lay, lay, fillvalue=[]):
-                if _fork and fork:
-                    dlay = _fork.comp_lay(fork, rn,root=root)
-                    dLay += [dlay]; Et+=dlay.Et  # combine input forks
-                else: dLay += [[]]
-            derH += [dLay]
+            if i:
+                dLay = []
+                for _fork, fork in zip_longest(_lay, lay, fillvalue=[]):
+                    if _fork and fork:
+                        dlay = _fork.comp_lay(fork, rn,root=root)
+                        dLay += [dlay]; Et+=dlay.Et  # combine input forks
+                    else: dLay += [[]]
+                derH += [dLay]
+                
+            else:  # single lay0
+                dlay = _lay.comp_lay(lay, rn,root=root)
+                derH += [[[],dlay]] if fd else  [[dlay,[]]]
     return derH
 
 def norm_H(H, n):
@@ -470,7 +487,7 @@ def norm_H(H, n):
 
 def L2N(link_,root):
     for L in link_:
-        L.root = root; L.fd_=copy(L.nodet[0].fd_); L.mL_t,L.rimt = [[],[]],[[],[]]; L.aRad=0; L.visited_,L.node_,L.link_,L.extH = [],[],[],[]
+        L.root = root; L.fd_=copy(L.nodet[0].fd_); L.mL_t,L.rimt = [[],[]],[[],[]]; L.aRad=0; L.visited_,L.node_,L.link_,L.extH = [],copy(L.nodet),[],[]  # L.node_ should be L>nodet?
 
 def frame2G(G, **kwargs):
     blob2G(G, **kwargs)
